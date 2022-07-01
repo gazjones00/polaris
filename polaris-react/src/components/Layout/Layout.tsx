f19ee9ea58b1bb9aecb28002f782ebc0bdc3af42
Kyle Durand <kyledurand@users.noreply.github.com>
2022-07-01 11:01:43 -0400

    Remove annotated mentions

--- polaris-react/src/components/Layout/Layout.tsx
+++ polaris-react/src/components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {Grid} from '../Grid';
 
-import {AnnotatedSection, Section} from './components';
+import {Section} from './components';
 
 export interface LayoutProps {
   /** Automatically adds sections to layout. */
@@ -12,12 +12,10 @@ export interface LayoutProps {
 }
 
 export const Layout: React.FunctionComponent<LayoutProps> & {
-  AnnotatedSection: typeof AnnotatedSection;
   Section: typeof Section;
 } = function Layout({sectioned, children}: LayoutProps) {
   const content = sectioned ? <Section>{children}</Section> : children;
   return <Grid>{content}</Grid>;
 };
 
-Layout.AnnotatedSection = AnnotatedSection;
 Layout.Section = Section;
